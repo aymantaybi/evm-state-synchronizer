b8310193a503009f7a40c3d9a4ba69ac0bcd9bed
aymantaybi <42820584+aymantaybi@users.noreply.github.com>
2024-11-19 00:08:36 +0100

    Update index.js

--- reader/index.js
+++ reader/index.js
@@ -15,12 +15,18 @@ db.on("error", (err) => {
 const stateRoot = "0xdd2b02b747fc61ac1d9a586324287a06f940dfbb370070f0f176685ac63c9029"; // 40021727
 const stateRootBuffer = Buffer.from(stateRoot.slice(2), "hex");
 
-const trie = new Trie(db, stateRootBuffer);
+const trie = new Trie(db);
 
 const address = "0xc1eb47de5d549d45a871e32d9d082e7ac5d2e3ed";
 const addressBuffer = Buffer.from(address.slice(2), "hex");
 
 (async function () {
+  let value = trie.get(stateRootBuffer);
+
+  console.log({ value });
+
+  return;
+
   const data = await trie.get(addressBuffer);
   console.log({ data });
   const acc = Account.fromAccountData(data);
