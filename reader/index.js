540f1ed04ce5a73589a294bea2b3e6ec87d74b73
aymantaybi <42820584+aymantaybi@users.noreply.github.com>
2024-11-18 23:50:59 +0100

    Update index.js

--- reader/index.js
+++ reader/index.js
@@ -2,7 +2,7 @@ const { Level } = require("level");
 const { Account, BN, bufferToHex, rlp } = require("ethereumjs-util");
 const { SecureTrie: Trie } = require("merkle-patricia-tree");
 
-const db = new Level("~/chaindata");
+const db = new Level("/chaindata");
 
 const stateRoot = "0xdd2b02b747fc61ac1d9a586324287a06f940dfbb370070f0f176685ac63c9029"; // 40021727
 const stateRootBuffer = Buffer.from(stateRoot.slice(2), "hex");
