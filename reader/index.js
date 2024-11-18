bbd9008a9553956350cc59ef5fb7e01b63e15560
aymantaybi <42820584+aymantaybi@users.noreply.github.com>
2024-11-18 23:58:01 +0100

    Update index.js

--- reader/index.js
+++ reader/index.js
@@ -1,8 +1,16 @@
 const { Level } = require("level");
 const { Account, BN, bufferToHex, rlp } = require("ethereumjs-util");
-const { SecureTrie: Trie } = require("merkle-patricia-tree");
+const { BaseTrie: Trie } = require("merkle-patricia-tree");
 
-const db = new Level("/chaindata");
+const db = new Level("../../chaindata");
+
+db.on("open", () => {
+  console.log("Database is open and ready for operations.");
+});
+
+db.on("error", (err) => {
+  console.error("Error occurred while opening the database:", err);
+});
 
 const stateRoot = "0xdd2b02b747fc61ac1d9a586324287a06f940dfbb370070f0f176685ac63c9029"; // 40021727
 const stateRootBuffer = Buffer.from(stateRoot.slice(2), "hex");
