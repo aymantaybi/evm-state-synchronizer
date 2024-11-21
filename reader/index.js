a5c319a66ebd08e3aefd73183943b926ce46d440
aymantaybi <42820584+aymantaybi@users.noreply.github.com>
2024-11-21 02:58:29 +0100

    Update index.js

--- reader/index.js
+++ reader/index.js
@@ -2,7 +2,7 @@ const { Level } = require("level");
 const { Account, BN, bufferToHex, rlp, keccak256, toBuffer } = require("ethereumjs-util");
 const { SecureTrie: Trie } = require("merkle-patricia-tree");
 
-const db = new Level("../../chaindata");
+const db = new Level("../roninchain/ronin/chaindata");
 
 db.on("open", () => {
   console.log("Database is open and ready for operations.");
@@ -12,16 +12,35 @@ db.on("error", (err) => {
   console.error("Error occurred while opening the database:", err);
 });
 
-const stateRoot = "0xdd2b02b747fc61ac1d9a586324287a06f940dfbb370070f0f176685ac63c9029"; // 40021727
+const stateRoot = "0xddc8f1b241f9090547501d92c2a943a41e8b076f14f2836be2cd8b4b1f6053c4"; // 40079306
 const stateRootBuffer = Buffer.from(stateRoot.slice(2), "hex");
 
 const trie = new Trie(db, stateRootBuffer);
 
+const address = "0xc1eb47de5d549d45a871e32d9d082e7ac5d2e3ed";
+const addressBuffer = Buffer.from(address.slice(2), "hex");
+
 (async function () {
-  trie
-    .createReadStream()
-    .on("data", console.log)
+  const data = await trie.get(addressBuffer);
+  const acc = Account.fromAccountData(data);
+
+  console.log("-------State-------");
+  console.log(`nonce: ${acc.nonce}`);
+  console.log(`balance in wei: ${acc.balance}`);
+  console.log(`storageRoot: ${bufferToHex(acc.stateRoot)}`);
+  console.log(`codeHash: ${bufferToHex(acc.codeHash)}`);
+
+  const storageTrie = trie.copy();
+  storageTrie.root = acc.stateRoot;
+
+  console.log("------Storage------");
+  const stream = storageTrie.createReadStream();
+  stream
+    .on("data", (data) => {
+      console.log(`key: ${bufferToHex(data.key)}`);
+      console.log(`Value: ${bufferToHex(rlp.decode(data.value))}`);
+    })
     .on("end", () => {
-      console.log("End.");
+      console.log("Finished reading storage.");
     });
 })();
